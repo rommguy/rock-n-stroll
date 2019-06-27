41f5d46f131f041e26ec56b4fbef4d5f4706edeb
rommguy <rommguy@gmail.com>
2019-06-27 16:46:44 +0300

    intro-carousel

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,6 +1,34 @@
 import React, { FunctionComponent } from 'react'
 import { RouteProps } from '../types'
+import {Carousel} from './Carousel'
+import StrollerIcon from '../icons/stroller-icon.svg'
+import './Intro.scss'
+
+const introSlides: IntroSlideProps[] = [{
+    icon:  StrollerIcon,
+    backgroundColor: 'red',
+    text: 'אני השקופית הראשונה'
+}, {
+    icon:  StrollerIcon,
+    backgroundColor: 'blue',
+    text: 'אני השקופית השניה'
+}, {
+    icon:  StrollerIcon,
+    backgroundColor: 'green',
+    text: 'אני השקופית השלישית'
+}]
+
+interface IntroSlideProps {
+    icon: any,
+    backgroundColor: string
+    text: string
+}
+
+const IntroSlide:FunctionComponent<IntroSlideProps> = props => (<div className="intro-slide" style={{backgroundColor: props.backgroundColor}}>
+        <img src={props.icon} className="slide-icon"/>
+        <div className="slide-text">{props.text}</div>
+    </div>)
 
 export const Intro: FunctionComponent<RouteProps> = () => {
-    return <div className="intro-root">I am intro</div>
+    return <div className="intro-root"><Carousel>{introSlides.map(slideDef => <IntroSlide {...slideDef}/>)}</Carousel></div>
 }
