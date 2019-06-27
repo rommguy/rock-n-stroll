9d2a783ac724a33acbbc43a4228c51108f22b869
rommguy <rommguy@gmail.com>
2019-06-27 18:55:35 +0300

    add material button. Navigate to map at the end of intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,3 +1,4 @@
+import * as H from 'history'
 import React, { FunctionComponent, useEffect, useState } from 'react'
 import { RouteProps } from '../types'
 import { Carousel } from './Carousel'
@@ -5,15 +6,17 @@ import landingIcon from '../icons/intro/landing-icon.svg'
 import icon2 from '../icons/intro/Icons-02.svg'
 import icon3 from '../icons/intro/Icons-03.svg'
 import icon4 from '../icons/intro/Icons-04.svg'
+import Button from '@material-ui/core/Button'
 import './Intro.scss'
 
 // ffd0d0
 
-const introSlides: IntroSlideProps[] = [
+const introSlides: IntroSlideDef[] = [
     {
         icon: icon4,
         backgroundColor: '#c5d7d1',
         text: 'היו חלק מקהילה אורבנית צומחת וגדלה',
+        buttonText: 'יאללה!',
     },
     {
         icon: icon2,
@@ -27,11 +30,16 @@ const introSlides: IntroSlideProps[] = [
     },
 ]
 
-interface IntroSlideProps {
+interface IntroSlideDef {
     icon: any
     backgroundColor: string
-    title?: string
     text: string
+    buttonText?: string
+}
+
+interface IntroSlideProps extends IntroSlideDef {
+    history: H.History
+    location: H.Location
 }
 
 const IntroSlide: FunctionComponent<IntroSlideProps> = props => (
@@ -40,17 +48,31 @@ const IntroSlide: FunctionComponent<IntroSlideProps> = props => (
         style={{ backgroundColor: props.backgroundColor }}
     >
         <img src={props.icon} className="slide-icon" alt="slide-icon" />
-        {props.title && <div className="slide-title">{props.title}</div>}
         <div className="slide-text">{props.text}</div>
+        {props.buttonText && (
+            <Button
+                className="go-button"
+                variant="contained"
+                color="primary"
+                onClick={() =>
+                    props.history.push({
+                        pathname: '/map',
+                        search: props.location.search,
+                    })
+                }
+            >
+                {props.buttonText}
+            </Button>
+        )}
     </div>
 )
 
-export const Intro: FunctionComponent<RouteProps> = () => {
+export const Intro: FunctionComponent<RouteProps> = props => {
     const [showSlide, setShowSlide] = useState<boolean>(false)
     useEffect(() => {
         setTimeout(() => {
             setShowSlide(true)
-        }, 5000)
+        }, 3000)
     }, [])
 
     return (
@@ -58,7 +80,12 @@ export const Intro: FunctionComponent<RouteProps> = () => {
             {showSlide ? (
                 <Carousel>
                     {introSlides.map((slideDef, i) => (
-                        <IntroSlide {...slideDef} key={i} />
+                        <IntroSlide
+                            {...slideDef}
+                            history={props.history}
+                            location={props.location}
+                            key={i}
+                        />
                     ))}
                 </Carousel>
             ) : (
