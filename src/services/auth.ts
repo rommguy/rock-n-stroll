600efef203adb8a4b2384d5fd170d4d6c0c51b85
user.email <omry@pop-os.localdomain>
2019-06-27 18:50:57 +0300

    added backend services

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,7 @@
 import {app} from './my-firebase';
 import {auth} from 'firebase';
 
-export const login =  () => {
-     app.auth().signInWithPopup(new auth.GoogleAuthProvider());
-    return app.auth().currentUser;
+export const login = async () => {
+    await app.auth().signInWithPopup(new auth.GoogleAuthProvider());
+    return app.auth().currentUser!;
 }
