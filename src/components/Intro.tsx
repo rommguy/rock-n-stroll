0cd6b87fc9b4a51fc749d67b28db0ba2d4545c4b
rommguy <rommguy@gmail.com>
2019-06-27 16:14:24 +0300

    add routes

---
+++ src/components/Intro.tsx
@@ -0,0 +1,6 @@
+import React, { FunctionComponent } from 'react'
+import { RouteProps } from '../types'
+
+export const Intro: FunctionComponent<RouteProps> = () => {
+    return <div className="intro-root">I am intro</div>
+}
