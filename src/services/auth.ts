4eb3411ca456d744b23352cdeb020412130a597f
Ido Rosenthal <idoros@gmail.com>
2019-06-28 11:13:18 +0300

    get logging from url

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,9 +1,10 @@
 import { app } from './my-firebase'
 import { auth } from 'firebase'
+import { getCurrentUserEmail } from './db'
 
 export const login = async () => {
     await app.auth().signInWithPopup(new auth.GoogleAuthProvider())
     return app.auth().currentUser!
 }
 
-export const currentUser = () => app.auth().currentUser
+export const currentUser = () => ({ email: getCurrentUserEmail() })
